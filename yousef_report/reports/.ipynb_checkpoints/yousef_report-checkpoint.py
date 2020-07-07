23c3c5ff664893ce2cb56a295466de22a1010004
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-07-07 11:43:58 +0000

    Report XML Edit

--- yousef_report/reports/.ipynb_checkpoints/yousef_report-checkpoint.py
+++ yousef_report/reports/.ipynb_checkpoints/yousef_report-checkpoint.py
@@ -4,6 +4,7 @@ from odoo.exceptions import Warning
 
 
 class YousefReport(models.AbstractModel):
+    
     _name = 'report.yousef_report.yreport'
     _inherit = 'report.report_xlsx.abstract'
     
