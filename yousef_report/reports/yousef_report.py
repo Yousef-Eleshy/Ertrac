2ec63f83f5a862086474373536516553c98a47ea
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-07-07 10:44:56 +0000

    Added Write

--- yousef_report/reports/yousef_report.py
+++ yousef_report/reports/yousef_report.py
@@ -6,3 +6,10 @@ from odoo.exceptions import Warning
 class YousefReport(models.AbstractModel):
     _name = 'report.yousef_report.yreport'
     _inherit = 'report.report_xlsx.abstract'
+    
+    def generate_xlsx_report(self, workbook, data, task_ids):
+        report_name = "Yousef Report"
+        # One sheet by partner
+        worksheet = workbook.add_worksheet(report_name[:31])
+        
+        worksheet.write('A1', 123)
