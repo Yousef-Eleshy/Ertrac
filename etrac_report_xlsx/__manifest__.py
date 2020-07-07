5b089e2c8f98c565678c0985a682d5744941a2a7
muhammedashraf9244 <hom9244.9244@gmail.com>
2020-07-07 09:56:48 +0000

    my change

--- etrac_report_xlsx/__manifest__.py
+++ etrac_report_xlsx/__manifest__.py
@@ -26,7 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-       'reports/report.xml',
+        'reports/report.xml',
     ], 
     # only loaded in demonstration mode
     'demo': [
--- etrac_report_xlsx/reports/.ipynb_checkpoints/firstreport-checkpoint.py
+++ etrac_report_xlsx/reports/.ipynb_checkpoints/firstreport-checkpoint.py
@@ -1,12 +1,15 @@
 
-
+import xlsxwriter
 from odoo import models, fields, api, _
 
 class etrac_first_report(models.AbstractModel):
     _name = 'report.etrac_report_xlsx.firstreport'
     _inherit = 'report.report_xlsx.abstract'
-    def generate_xlsx_report(self, workbook, data, task_ids):
+    
+    def generate_xlsx_report(self, workbook, data):
         report_name = "إتقرير حصر الموظفين "
         # One sheet by partner
-        worksheet = workbook.add_worksheet('تقرير حصر الموظفين')
-        
+        #workbook   = xlsxwriter.Workbook('report_employees.xlsx')
+        #format1= workbook.add_format({'font size':14,'bold':True})
+        #sheet1 = workbook.add_worksheet('تقرير حصر الموظفين')
+        #sheet1.write(2,2,'Name',format1)
