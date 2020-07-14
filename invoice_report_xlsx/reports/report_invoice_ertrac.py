6470fa8b1a55730b1f637366afdfe6cf8fd42e18
dinamohammed <dina.mohammed392@gmail.com>
2020-07-14 14:19:18 +0000

    Add multiple values fitted

--- invoice_report_xlsx/reports/report_invoice_ertrac.py
+++ invoice_report_xlsx/reports/report_invoice_ertrac.py
@@ -2,12 +2,13 @@
 from odoo import models, fields, api, _
 import io
 import base64
+import math
 
 class TaskErtracXlsxs(models.AbstractModel):
     _name = 'report.report_invoice_ertrac.report_invoice_ertrac'
     _inherit = 'report.report_xlsx.abstract'
 
-    def generate_xlsx_report(self, workbook, data, task_ids):
+    def generate_xlsx_report(self, workbook, data, invoice_ids):
         report_name = "مستخلص"
         # One sheet by partner
         worksheet = workbook.add_worksheet(report_name[:31])
@@ -22,31 +23,35 @@ class TaskErtracXlsxs(models.AbstractModel):
         
         worksheet.right_to_left()
         worksheet.set_column('A:A', 5)
-        worksheet.set_column('B:B', 50)
-        worksheet.set_column('C:X', 20)
+        worksheet.set_column('E:F', 5)
+        worksheet.set_column('H:K', 5)
+        worksheet.set_column('B:B', 20)
+        worksheet.set_column('C:D', 10)
+        worksheet.set_column('G:G', 10)
+        worksheet.set_column('L:L', 10)
         bold = workbook.add_format({'bold': True})
-        bold.set_font_size(14)
+        bold.set_font_size(12)
         bold_center = workbook.add_format({'bold': True, 'align': 'center'})
-        bold_center.set_font_size(14)
+        bold_center.set_font_size(12)
         bold_right = workbook.add_format({'bold': True, 'align': 'right'})
-        bold_right.set_font_size(14)
+        bold_right.set_font_size(12)
         
         # Company Logo
-        company_logo = self.env.user.company_id.logo
-        imgdata = base64.b64decode(company_logo)
-        image = io.BytesIO(imgdata)
-        worksheet.insert_image('D1', 'myimage.png', {'image_data': image,'x_scale': 1.2, 'y_scale': 0.9})
+#         company_logo = self.env.user.company_id.logo
+#         imgdata = base64.b64decode(company_logo)
+#         image = io.BytesIO(imgdata)
+#         worksheet.insert_image('D1', 'myimage.png', {'image_data': image,'x_scale': 1.2, 'y_scale': 0.9})
         
-        worksheet.merge_range(4, 1, 4, 4, "سكك حديد مصر - عموم هندسة السكة", bold_center)
-        worksheet.merge_range(6, 0, 6, 3, "بيان مرفق بفاتورة رقم       /2020", bold_right)
-        worksheet.merge_range(7, 1, 7, 4, "طبقا للعقد رقم (1) لسنة 2017/2018", bold_right)
-        worksheet.merge_range(6, 6, 6, 8, ".................. : مبلغ الإعتماد", bold_right)
-        worksheet.merge_range(7, 6, 7, 8, ".................. : رقم وتاريخ الإعتماد", bold_right)
-        worksheet.merge_range(8, 6, 8, 8, ".................. : قيمة العقد", bold_right)
+        worksheet.merge_range(4, 1, 4, 6, "سكك حديد مصر - عموم هندسة السكة", bold_center)
+        worksheet.merge_range(6, 0, 6, 5, "بيان مرفق بفاتورة رقم       /2020", bold_right)
+        worksheet.merge_range(7, 1, 7, 6, "طبقا للعقد رقم (1) لسنة 2017/2018", bold_right)
+        worksheet.merge_range(6, 8, 6, 11, ".................. : مبلغ الإعتماد", bold_right)
+        worksheet.merge_range(7, 8, 7, 11, ".................. : رقم وتاريخ الإعتماد", bold_right)
+        worksheet.merge_range(8, 8, 8, 11, ".................. : قيمة العقد", bold_right)
         
-        worksheet.merge_range(9, 1, 9, 4, " اسم العملية : الأعمال التي قامت بتنفيذها الشركة المصرية لتجديد و صيانة خطوط السكك الحديدية", bold_center)
-        worksheet.merge_range(10, 1, 10, 4, "أعمال الصيانة الميكانيكية ما بين        بالخط الطالع والنازل      التابع لإدارة هندسة    خلال شهر   ", bold_center)
-        worksheet.merge_range(11, 1, 11, 2, " %s %s هندسة القاهرة خلال شهر" % (fields.Date.today().strftime("%B"),
+        worksheet.merge_range(9, 1, 9, 9, " اسم العملية : الأعمال التي قامت بتنفيذها الشركة المصرية لتجديد و صيانة خطوط السكك الحديدية", bold_center)
+        worksheet.merge_range(10, 1, 10, 8, "أعمال الصيانة الميكانيكية ما بين        بالخط الطالع والنازل      التابع لإدارة هندسة    خلال شهر   ", bold_center)
+        worksheet.merge_range(11, 1, 11, 5, " %s %s هندسة القاهرة خلال شهر" % (fields.Date.today().strftime("%B"),
                                                                                fields.date.today().strftime("%Y")), bold_center)
         worksheet.write(12, 0, "هندسة 207")
 #         worksheet.write(12, 8, "مطابع السكك الحديد 2240//1996/25000")
@@ -57,52 +62,64 @@ class TaskErtracXlsxs(models.AbstractModel):
                                                'border': 1})
         cell_format_total = workbook.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter',
                                                   'border': 1, 'fg_color': '#FFC7CE'})
-        cell_format_total2 = workbook.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter',
-                                                  'border': 1, 'fg_color': '#C6EFCE'})
+        cell_format_row_wrap = workbook.add_format({'bold': False, 'align': 'center', 'valign': 'vcenter',
+                                               'border': 1})
+        
+        cell_format_row_wrap.set_text_wrap()
         cell_format_header.set_center_across()
         
-        worksheet.merge_range(14, 0, 15, 0, 'رقم بند العقد')
+        cell_format_header_wrap = workbook.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter',
+                                                  'border': 1, 'fg_color': '#faf200'})
+        cell_format_header_wrap.set_text_wrap()
+        
+        worksheet.merge_range(14, 0, 15, 0, 'رقم بند العقد', cell_format_header_wrap)
         worksheet.merge_range(14, 1, 15, 1, 'بيان مفردات الأعمال', cell_format_header)
-        worksheet.merge_range(14, 2, 15, 2, 'الوحدة', cell_format_header)
-        worksheet.merge_range(14, 3, 15, 3, 'الكمية', cell_format_header)
-        worksheet.merge_range(14, 4, 14, 5, 'الثمن بالوحدة', cell_format_header)
-        worksheet.write(15, 4, 'قرش', cell_format_header)
-        worksheet.write(15, 5, 'جنية', cell_format_header)
-        worksheet.merge_range(14, 6, 15, 7, 'النسبة', cell_format_header)
-        worksheet.merge_range(14, 8, 14, 9, 'تكاليف كل بند', cell_format_header)
-        worksheet.write(15, 8, 'قرش', cell_format_header)
-        worksheet.write(15, 9, 'جنية', cell_format_header)
-        worksheet.merge_range(14, 10, 14, 11, 'المبلغ المصرح بصرفه', cell_format_header)
-        worksheet.write(15, 10, 'قرش', cell_format_header)
-        worksheet.write(15, 11, 'جنية', cell_format_header)
-        worksheet.merge_range(14, 12, 15, 13, 'ملاحظات', cell_format_header)
+        worksheet.merge_range(14, 2, 15, 2, 'الوحدة', cell_format_header_wrap)
+        worksheet.merge_range(14, 3, 15, 3, 'الكمية', cell_format_header_wrap)
+        worksheet.merge_range(14, 4, 14, 5, 'الثمن بالوحدة', cell_format_header_wrap)
+        worksheet.write(15, 4, 'قرش', cell_format_header_wrap)
+        worksheet.write(15, 5, 'جنية', cell_format_header_wrap)
+        worksheet.merge_range(14, 6, 15, 6, 'النسبة', cell_format_header_wrap)
+        worksheet.merge_range(14, 7, 14, 8, 'تكاليف كل بند', cell_format_header_wrap)
+        worksheet.write(15, 7, 'قرش', cell_format_header_wrap)
+        worksheet.write(15, 8, 'جنية', cell_format_header_wrap)
+        worksheet.merge_range(14, 9, 14, 10, 'المبلغ المصرح بصرفه', cell_format_header_wrap)
+        worksheet.write(15, 9, 'قرش', cell_format_header_wrap)
+        worksheet.write(15, 10, 'جنية', cell_format_header_wrap)
+        worksheet.merge_range(14, 11, 15, 11, 'ملاحظات', cell_format_header)
         row = 16
 
         for idx , invoice in enumerate(invoice_ids):
             for idxx , invoice_line_ids in enumerate(invoice.invoice_line_ids):
-                row += 1
+                unit_tuple = math.modf(invoice_line_ids.price_unit)
+                subtotal_tuple = math.modf(invoice_line_ids.price_subtotal)
+                allowed_tuple = math.modf(invoice_line_ids.allowed_amount)
                 col = 0
-                worksheet.write(row, col, invoice_line_ids.product_id.default_code , cell_format_row)
+                worksheet.write(row, col, invoice_line_ids.product_id.default_code , cell_format_row_wrap)
                 col += 1
-                worksheet.write(row, col, invoice_line_ids.product_id.name, cell_format_row)
+                worksheet.write(row, col, invoice_line_ids.product_id.name, cell_format_row_wrap)
                 col += 1
-                worksheet.write(row, col, invoice_line_ids.product_uom_id.name ,cell_format_row)
+                worksheet.write(row, col, invoice_line_ids.product_uom_id.name ,cell_format_row_wrap)
                 col += 1
-                worksheet.write(row, col, invoice_line_ids.quantity , cell_format_row)
+                worksheet.write(row, col, invoice_line_ids.quantity , cell_format_row_wrap)
                 col += 1
-                worksheet.write(row, col, invoice_line_ids.price_unit, cell_format_row)
+                worksheet.write(row, col, int(unit_tuple[0]), cell_format_row_wrap)
                 col += 1
+                worksheet.write(row, col, int(invoice_line_ids.price_unit), cell_format_row_wrap)
                 col += 1
-                worksheet.write(row, col, invoice_line_ids.rated, cell_format_row)
+                worksheet.write(row, col, invoice_line_ids.rated*100, cell_format_row_wrap)
                 col += 1
-                worksheet.write(row, col, invoice_line_ids.price_subtotal, cell_format_row)
+                worksheet.write(row, col, int(subtotal_tuple[0]), cell_format_row_wrap)
                 col += 1
+                worksheet.write(row, col, int(invoice_line_ids.price_subtotal), cell_format_row_wrap)
                 col += 1
-                worksheet.write(row, col, invoice_line_ids.allowed_amount, cell_format_row)  
+                worksheet.write(row, col, int(allowed_tuple[0]), cell_format_row_wrap)  
                 col += 1
+                worksheet.write(row, col, int(invoice_line_ids.allowed_amount), cell_format_row_wrap)  
                 col += 1
                 worksheet.write(row, col, invoice_line_ids.disc ,cell_format_row)
                 col += 1
+                row += 1
 #              worksheet.merge_range(row_initial+1, 1, row , 1, 'الخط الطالع',cell_format_row)
 #              # Final Total
 #              row += 1
@@ -121,15 +138,15 @@ class TaskErtracXlsxs(models.AbstractModel):
         row += 6
         # Footer
         row += 2
-        worksheet.merge_range(row, 0, row, 5, "جميع الأعمال تمت طبقا للأصول الفنية للهيئة وبحالة جيدة", bold_center)
+        worksheet.merge_range(row, 2, row, 8, "جميع الأعمال تمت طبقا للأصول الفنية للهيئة وبحالة جيدة", bold_center)
         row += 1
         worksheet.write(row, 1, "مندوب الشركه", bold_center)
         row += 1
-        worksheet.merge_range(row-1, 4, row-1, 5, "مندوب الهيئه", bold_center)
+        worksheet.merge_range(row-1, 7, row-1, 11, "مندوب الهيئه", bold_center)
 
         row += 4
-        worksheet.merge_range(row, 1, row, 6, "يعتمد / ", bold_center)
+        worksheet.merge_range(row, 2, row, 6, "يعتمد / ", bold_center)
         row += 1
-        worksheet.merge_range(row, 1, row, 6,"رئيس مجلس الاداره والعضو المنتدب ", bold_center)
+        worksheet.merge_range(row, 2, row, 6,"رئيس مجلس الاداره والعضو المنتدب ", bold_center)
         row += 1
-        worksheet.merge_range(row, 1, row, 6,"مهندس / مصطفى عبداللطيف أبوالمكارم ", bold_center)
+        worksheet.merge_range(row, 2, row, 6,"مهندس / مصطفى عبداللطيف أبوالمكارم ", bold_center)
