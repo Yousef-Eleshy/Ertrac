404fed9735b37b5ac2b7479fa4d5c8015ae403bd
dinamohammed <dina.mohammed392@gmail.com>
2020-07-29 12:04:39 +0000

    On hold until osama answers with mail

--- report_3_xlsx/reports/.ipynb_checkpoints/report-checkpoint.py
+++ report_3_xlsx/reports/.ipynb_checkpoints/report-checkpoint.py
@@ -25,6 +25,7 @@ class ReportErtrac(models.AbstractModel):
         worksheet.right_to_left()
         worksheet.set_column('A:A', 2)
         worksheet.set_column('B:B', 20)
+        worksheet.set_column('F:F', 20)
         worksheet.set_column('C:E', 10)
         bold = workbook.add_format({'bold': True})
         bold.set_font_size(12)
@@ -33,16 +34,21 @@ class ReportErtrac(models.AbstractModel):
         bold_right = workbook.add_format({'bold': True, 'align': 'right'})
         bold_right.set_font_size(12)
         
+        previous_year = (int(fields.date.today().strftime("%Y")) - 1)
         # Company Logo
         company_logo = self.env.user.company_id.logo
         imgdata = base64.b64decode(company_logo)
         image = io.BytesIO(imgdata)
         worksheet.insert_image('B1', 'myimage.png', {'image_data': image,'x_scale': 1, 'y_scale': 0.5})
 
-        worksheet.merge_range(4, 1, 4, 6, "%s طبقا لخطة الهيئة عام تقرير %s تقدم العمل بالتجديدات خلال شهر " % (fields.Date.today().strftime("%B"), fields.date.today().strftime("%Y")),bold_center)
+        worksheet.merge_range(4, 1, 4, 6, "%s / %s طبقا لخطة الهيئة عام %s  تقرير تقدم العمل بالتجديدات خلال شهر " 
+                              %(previous_year,fields.date.today().strftime("%Y"),
+                                fields.Date.today().strftime("%B")),bold_center)
         
-        cell_format_header = workbook.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter',
-                                                  'border': 1, 'fg_color': '#faf200'})
+        cell_format_header = workbook.add_format({'bold': False, 'align': 'center', 'valign': 'vcenter',
+                                                  'border': 1,})
+        cell_format_header_2 = workbook.add_format({'bold': False, 'align': 'center', 'valign': 'vcenter',
+                                                  'border': 1,})
         cell_format_row = workbook.add_format({'bold': False, 'align': 'center', 'valign': 'vcenter',
                                                'border': 1})
         cell_format_total = workbook.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter',
@@ -53,14 +59,38 @@ class ReportErtrac(models.AbstractModel):
         cell_format_row_wrap.set_text_wrap()
         cell_format_header.set_center_across()
         
+        cell_format_header.set_font_size(16)
+        cell_format_header_2.set_font_size(12)
+        
         cell_format_header_wrap = workbook.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter',
                                                   'border': 1, 'fg_color': '#faf200'})
         cell_format_header_wrap.set_text_wrap()
         
-        worksheet.write(5, 1, 'ملاحظات', cell_format_header)
-        worksheet.write(5, 2, 'اجمالى المنفذ', cell_format_header)
-        worksheet.write(5, 3, 'المنفذ خلال الشهر', cell_format_header)
-        worksheet.write(5, 4, 'المقرر', cell_format_header)
-        worksheet.write(5, 5, 'موقع العمل', cell_format_header)
-
         row = 6
+        tasks = []
+        # arrange timesheets according to tasks 
+        for idx , timesheet in enumerate(timesheet_ids):
+            tasks.append(timesheet.task_id.name)
+        
+        taskss = list(dict.fromkeys(tasks))
+        
+#         for task in taskss :
+#             worksheet.merge_range(row , 1 , row , 5 , task , bold_right)
+#             row += 1
+#             worksheet.write(row, 5, 'ملاحظات', cell_format_header)
+#             worksheet.write(row, 4, 'اجمالى المنفذ', cell_format_header)
+#             worksheet.write(row, 3, 'المنفذ خلال الشهر', cell_format_header_2)
+#             worksheet.write(row, 2, 'المقرر', cell_format_header)
+#             worksheet.write(row, 1, 'موقع العمل', cell_format_header)
+#             row + = 1 
+#             for idx , timesheet in enumerate(timesheet_ids):
+                
+#                 if task == timesheet.task_id.name:
+#                     timesheet.project_id.name
+#                     timesheet.task_id.planned_hours
+             
+                 
+
+        
+        
+        
