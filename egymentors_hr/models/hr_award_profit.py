595cdb35921e90a955de073a8c3db9d49ea3f735
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-08-12 14:01:59 +0000

    Fix

--- egymentors_hr/models/hr_award_profit.py
+++ egymentors_hr/models/hr_award_profit.py
@@ -59,22 +59,6 @@ class HrAwardProfit(models.Model):
 
 	total_amount = fields.Float("Total", compute=_get_total)
     
-	@api.onchange('award_value')
-    def change_award_value(self):
-        for emp_id in emp_ids:
-            if emp_id.id not in emps_list:
-                amount = 0.0
-                if rec.extra_type == 'award':
-                    amount = rec.award_value
-                if rec.extra_type == 'profit' and emp_id.previous_wage:
-                    amount = emp_id.previous_wage * rec.num_months
-                rec.line_ids.create({
-                    'award_profit_id': rec.id,
-                    'employee_id': emp_id.id,
-                    'amount': amount
-                })    
-    
-    
     
 	@api.onchange('work_location_id')
 	def generate_employee_ids(self):
