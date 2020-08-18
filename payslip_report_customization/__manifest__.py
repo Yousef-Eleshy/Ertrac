53b350f95a39827967da29ec0f4ae3feea021e32
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-08-18 15:40:13 +0000

    Progress Commit

--- payslip_report_customization/__manifest__.py
+++ payslip_report_customization/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        #'reports/reports.xml',
         'reports/payslip_report_inherit.xml',
     ],
     # only loaded in demonstration mode
