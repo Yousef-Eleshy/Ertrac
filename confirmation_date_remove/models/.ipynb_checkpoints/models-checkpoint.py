5c3c817d1a6661990f9d637122a77daf3a1e5dbb
dinamohammed <dina.mohammed392@gmail.com>
2021-01-27 12:15:20 +0000

    Nothing

--- confirmation_date_remove/models/.ipynb_checkpoints/models-checkpoint.py
+++ confirmation_date_remove/models/.ipynb_checkpoints/models-checkpoint.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
     
-    date_approve = fields.Datetime('Confirmation Date', index=True, copy=False)
+    date_approve = fields.Datetime('Confirmation Date', readonly=0, index=True, copy=False)
